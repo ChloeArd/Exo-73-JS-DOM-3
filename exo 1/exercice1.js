688b3113f80c7256623db1fb10bc6776e45f9d6b
ChloeArd <chloe.ardoise@gmail.com>
2020-12-04 10:32:33 +0100

    Exercice 73

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,5 +1,13 @@
 let avatarEls = document.getElementsByTagName("img");
 
+for (let i = 0; i < avatarEls.length; i++) {
+    avatarEls.item(i).style.height = 300 + "px";
+}
+
+avatarEls.addEventListener("mouseover", function (){
+    document.getElementsByTagName('img').innerHTML = "Tu as survolé l'image";
+})
+
 
 
 
--- exo 4/exercice.js
+++ exo 4/exercice.js
@@ -1,4 +1,11 @@
+let monDiv = document.createElement('div');
+monDiv.className = 'planet';
+monDiv.style.backgroundColor = 'blue';
+document.body.appendChild(monDiv);
 
+let autreDiv = document.createElement('div');
+autreDiv.className = 'moon';
+monDiv.appendChild(autreDiv);
 
 
 
