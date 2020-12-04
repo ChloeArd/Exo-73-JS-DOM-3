c9b14d70351b1f32cae0387bbdb6bce3ef9ac8a6
ChloeArd <chloe.ardoise@gmail.com>
2020-12-04 15:07:30 +0100

    Exercice 73

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -2,11 +2,10 @@ let avatarEls = document.getElementsByTagName("img");
 
 for (let i = 0; i < avatarEls.length; i++) {
     avatarEls.item(i).style.height = 300 + "px";
+    avatarEls.item(i).setAttribute("title", "photo");
 }
 
-avatarEls.addEventListener("mouseover", function (){
-    document.getElementsByTagName('img').innerHTML = "Tu as survolé l'image";
-})
+
 
 
 
